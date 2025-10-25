599c1e85c37ef74d4a16a286613e81585943888f
Camila Valdrigues Candido <camila.candido@ndd.tech>
2025-10-24 22:07:35 -0300

    Movido páginas para diretorio raiz

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
-//const API_BASE_URL = "https://projetointegrador-wl8d.onrender.com"; // URL da api hospedada no Render
-const API_BASE_URL = "http://127.0.0.1:5000" // URL para testes locais
+const API_BASE_URL = "https://projetointegrador-wl8d.onrender.com"; // URL da api hospedada no Render
+//const API_BASE_URL = "http://127.0.0.1:5000" // URL para testes locais
 export { API_BASE_URL };
